644ff215f7b7237cea829dc324ad73a2c0040af5
muichc <cindy.ch.mui@gmail.com>
2022-03-16 16:52:37 -0700

    Implements endpoint to chnage password

--- server/src/api/controllers/auth.ts
+++ server/src/api/controllers/auth.ts
@@ -20,15 +20,17 @@ const register = async (req : Request, res : Response) : Promise<void> => {
 
 const login = async ( req : Request, res : Response ) : Promise<void> => {
     const user = await db.user.findOne({
-        where: {email: req.body.email}
+        where: {email: req.body.email},
+        include: [db.category]
     })
+    console.log(user.categories)
     console.log("logging in")
     if (!user){
         res.status(400).json({ message: "User does not exist"})
     } else {
         const match = await bcrypt.compare(req.body.password, user.password) 
         if (match) {
-            res.status(200).json({ status: 200, message: "Success, user is logged in", user: req.body.email})
+            res.status(200).json({ status: 200, message: "Success, user is logged in", user: req.body.email, categories: user.categories})
         } else {
             res.status(400).json({ message: "Email or password is incorrect"})
         }
@@ -36,6 +38,28 @@ const login = async ( req : Request, res : Response ) : Promise<void> => {
     }
 }
 
+const changePassword = async ( req : Request, res : Response ) : Promise<void> => {
+    console.log(req.body)
+    const user = await db.user.findOne({
+        where: {email: req.body.email}
+    })
+    if (!user){
+        res.status(400).json({ message: "User does not exist"})
+    } else {
+        const match = await bcrypt.compare(req.body.password, user.password)
+        if (match) {
+            const [...updated] = await db.user.update({password: bcrypt.hashSync(req.body.newPassword, 12)}, {where:{email:user.email}})
+            if (updated) {
+                res.status(200).json({ status: 200, message: "Success, user password changed", user: req.body.email})
+            } else {
+                res.status(500) .json({ status: 500, message: "Something went wrong server side", user: req.body.email})
+            }
+        } else {
+            res.status(400).json({message: "Password does not match", user: req.body.email})
+        }
+    }
+}
+
 module.exports = {
-    login, register
+    login, register, changePassword
 }
--- server/src/api/routes/auth.ts
+++ server/src/api/routes/auth.ts
@@ -3,5 +3,6 @@ const controller = require('../controllers')
 
 router.post('/register', controller.auth.register)
 router.post('/login', controller.auth.login)
+router.put('/changepw', controller.auth.changePassword)
 
 export = router
