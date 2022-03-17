5bdbe8aaae5fedaf376ecf5a03fa4a7da9947fcf
muichc <cindy.ch.mui@gmail.com>
2022-03-16 23:53:06 -0700

    Cleans up and refactors news show all endpoint and adds a search endpoint

--- server/src/api/controllers/news.ts
+++ server/src/api/controllers/news.ts
@@ -3,22 +3,26 @@ import fetch from 'node-fetch';
 
 
 const show = async (req: Request, res: Response) : Promise<void> => {
-    const requestHeaders : HeadersInit = {'X-Api-Key' : process.env.NEWS_API_KEY || 'cf2c7debcade4a3894af071f0f56c5ed'};
-    let url : string = process.env.NEWS_API_URL_SEARCH || '';
-    const response = await fetch(`${url}q=apple&from=2022-03-13&to=2022-03-13&sortBy=popularity`, {
+    const requestHeaders : HeadersInit = {'X-Api-Key' : process.env.NEWS_API_KEY || ''};
+    let url : string = process.env.NEWS_API_URL_TOP || 'https://newsapi.org/v2/top-headlines?';
+    const response = await fetch(`${url}country=us`, {
         headers: requestHeaders
     })
     const parsedResponse = await response.json()
-    // let urlTop : string = process.env.NEWS_API_URL_TOP || '';
-    // const respTop = await fetch(`${urlTop}country=us&category=business`, {
-    //     headers: requestHeaders
-    // })
-    // const parsedResp = await respTop.json()
-    // res.send(parsedResp)
-    // res.send(parsedResponse)
+    res.status(200).json({response: parsedResponse}) 
+}
+
+const search = async (req: Request, res: Response) : Promise<void> => {
+    const requestHeaders : HeadersInit = {'X-Api-Key' : process.env.NEWS_API_KEY || ''};
+    let url : string = process.env.NEWS_API_URL_SEARCH || 'https://newsapi.org/v2/top-headlines?';
+    const response = await fetch(`${url}country=us&q=${req.body.searchQuery}`, {
+        headers: requestHeaders
+    })
+    const parsedResponse = await response.json()
     res.status(200).json({response: parsedResponse}) 
 }
 
 module.exports = {
-    show
+    show,
+    search
 }
