c06db4fac6c3587d5ff47aab281c63e0f0e32b31
muichc <cindy.ch.mui@gmail.com>
2022-03-16 19:47:33 -0700

    Adds MUI components to style for header and news components

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,28 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 const Header = () => {
-    let loggedIn;
-    if(localStorage.getItem('user')) {
-        loggedIn = true;
-    } else {
-        loggedIn = false;
-    }
+
+    let loggedIn =localStorage.getItem('user')
     const navigate = useNavigate();
 
-    const handleLogOut = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchor)
+    const handleClick = (event : React.MouseEvent<HTMLButtonElement>) => {
+        setAnchor(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchor(null);
+    }
+
+    const handleUserProfile = () => {
+        setAnchor(null);
+        navigate('/profile')
+    }
+
+    const handleRegister = () => {
+        setAnchor(null);
+        navigate('/register')
+    }
+
+    const handleLogOut = () => {
         localStorage.removeItem('user')
+        setAnchor(null);
         navigate('/')
     }
 
-    return (
-        <div>
-            { loggedIn && 
-                <button onClick = {handleLogOut}> Log Out </button> 
-            }
-            
-        </div>
-    )
+    if (loggedIn) {
+        return (
+            <div>
+                <h1>Fake News Times</h1>
+                <IconButton
+                    id="menu-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchor}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                    'aria-labelledby': 'menu-button',
+                    }}
+                >
+                    <MenuItem onClick={handleUserProfile}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+                </Menu>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>Fake News Times</h1>
+                <a href='/login'>Login</a>
+                <IconButton
+                    id="menu-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchor}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                    'aria-labelledby': 'menu-button',
+                    }}
+                >
+                    <MenuItem onClick={handleRegister}>Register</MenuItem>
+                </Menu>
+            </div>
+        )
+    }
+
+    
 }
 
 export { Header }
--- client/src/components/News.tsx
+++ client/src/components/News.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { NewsData } from '../global/types'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const News = () => {
+const News = (newsArticle: NewsData) => {
     return (
-        <div>
-            
-        </div>
+        <Card sx={{ maxWidth: 350 }}>
+            <CardMedia
+                component="img"
+                height="200"
+                image={`${newsArticle.urlToImage}`}
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {newsArticle.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {newsArticle.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" href={`${newsArticle.url}`}>See full article</Button>
+            </CardActions>
+        </Card>
     )
 }
 
-export default News
+export { News }
