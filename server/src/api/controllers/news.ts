f3a2d19622a721eb6760acffe58835dc255e9430
muichc <cindy.ch.mui@gmail.com>
2022-03-18 19:29:16 -0700

    Implments ability to fetch by both categories and countries in showByUser endpoint

--- server/src/api/controllers/news.ts
+++ server/src/api/controllers/news.ts
@@ -1,6 +1,7 @@
 import type {Request, Response} from 'express';
 import fetch from 'node-fetch';
 
+const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology' ]
 
 const show = async (req: Request, res: Response) : Promise<void> => {
     const requestHeaders : HeadersInit = {'X-Api-Key' : process.env.NEWS_API_KEY2 || ''};
@@ -33,9 +34,15 @@ const showByCategory = async (req: Request, res: Response) : Promise<void> => {
 }
 
 const showByUser = async (req: Request, res: Response) : Promise<void> => {
-    let categoryQuery = ''
+    console.log('categories are', req.body)
+    let categoryQuery = '';
+    let countryQuery = [];
     for (let i in req.body) {
-        categoryQuery += `&category=${req.body[i]}`
+        if (categories.includes(req.body[i])){
+            categoryQuery += `&category=${req.body[i]}`
+        } else {
+            countryQuery.push(req.body[i])
+        }
     }
     const requestHeaders : HeadersInit = {'X-Api-Key' : process.env.NEWS_API_KEY2 || ''};
     let url: string = process.env.NEWS_API_URL_TOP || 'https://newsapi.org/v2/top-headlines?';
@@ -43,6 +50,14 @@ const showByUser = async (req: Request, res: Response) : Promise<void> => {
         headers: requestHeaders
     })
     const parsedResponse = await response.json()
+    for (let i in countryQuery) {
+        const countryResponse = await fetch(`${url}country=${countryQuery[i]}&pageSize=5`, {
+            headers: requestHeaders
+        })
+        const parsedCountryResponse = await countryResponse.json()
+        parsedResponse.totalResults += parsedCountryResponse.totalResults
+        parsedResponse.articles.push([...parsedCountryResponse.articles])
+    }
     res.status(200).json({response:parsedResponse})
 
 }
