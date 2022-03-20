db6c542e5e1bed8f177cae0642a3b8e0713ddfc9
muichc <cindy.ch.mui@gmail.com>
2022-03-19 18:26:19 -0700

    Adds script settings for heroku

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { routes } from './api/routes'
+import path from 'path'
 
 
 dotenv.config()
@@ -9,6 +10,7 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(express.json())
 
 app.use(cors())
@@ -17,5 +19,8 @@ app.use('/news', routes.news)
 app.use ('/auth', routes.auth)
 app.use('/category', routes.category)
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
